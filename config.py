854d639732f3fd28251cc19e592d94965af29ab6
AnyamaneePloy <137474486+AnyamaneePloy@users.noreply.github.com>
2024-03-20 16:56:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,7 @@
 pathPRGList = r"D:\Users\anyamanee\Anyamanee_Work\98_Git\C_DockerAirflow\20240320_PRGTableList.csv"
 
 # PRG Database connection string
-DB_CONNECTION_STRING = '''Driver={SQL Server};
-Server=192.168.102.1;
-Database=PRGDB20180101; 
-UID=mbk_navstaging;
-PWD=qe2#HsGq*'''
+DB_CONNECTION_STRING = '''Fill Config'''
 
 # SQL query (You can define multiple queries as needed)
 SQL_QUERY_All = '''SELECT * FROM {} where  year([Create Date CRM]) >= 2020'''  # Using {} as a placeholder for table name
